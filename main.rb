dawidd6/action-homebrew-bump-formula

main: always tap homebrew/core
for now at least

main.rb
@@ -73,12 +73,11 @@ module Homebrew
   git 'config', '--global', 'user.name', user_name
   git 'config', '--global', 'user.email', user_email
 
-  # Tap the correct tap
-  if tap.blank?
-    brew 'tap', 'homebrew/core'
-  else
-    brew 'tap', tap
-  end
+  # Always tap homebrew/core, otherwise brew can't find formulae
+  brew 'tap', 'homebrew/core'
+  
+  # Tap the requested tap if applicable
+  brew 'tap', tap unless tap.blank?
 
   # Append additional PR message
   message = if message.blank?

eugenesvk/action-homebrew-bump-cask

main.rb
@@ -84,12 +84,10 @@ module Homebrew
   git 'config', '--global', 'user.name' , user_name
   git 'config', '--global', 'user.email', user_email
 
-  # Tap the correct tap
-  if tap_path.blank?
-    brew 'tap', 'homebrew/core'
-  else
-    brew 'tap', tap_path
-  end
+  # Always tap homebrew/core, otherwise brew can't find formulae
+  brew 'tap', 'homebrew/core'
+  # Tap the requested tap if applicable
+  brew 'tap', tap_path unless tap_path.blank?
 
   # Append additional PR message
   message = if message.blank?